7bf23d47c82efad5f125bcb13922c1745e912cf5
Puslore <mini.big10@mail.ru>
2026-02-05 15:18:03 +0800

    add some tests

---
+++ test.py
@@ -0,0 +1,31 @@
+from script import sum, divide
+
+
+def test_sum():
+    a = 1
+    b = 2
+    assert sum(a, b) == 3
+
+
+def test_divide():
+    a = 2
+    b = 4
+    result = 0.5
+    assert divide(a, b) == result
+
+
+def test_divide_by_zero():
+    a = 2
+    b = 0
+    try:
+        divide(a, b)
+        assert False
+    
+    except ValueError as err:
+        print('Test (division by zero) passed successfully')
+
+
+if __name__ == "__main__":
+    test_divide()
+    test_sum()
+    test_divide_by_zero()
