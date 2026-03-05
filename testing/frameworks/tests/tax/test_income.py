c9a0ff319506885dc7e622a048d133475da44777
Puslore <mini.big10@mail.ru>
2026-03-05 15:23:51 +0800

    done task

--- testing/frameworks/tests/tax/test_income.py
+++ testing/frameworks/tests/tax/test_income.py
@@ -1,10 +1,25 @@
-from testing.frameworks.src.tax.income import calculate_tax
+import pytest
+from tax.income import calculate_tax
 
 
 def test_icome_tax():
 
-    assert calculate_tax(100) == 15
+    assert calculate_tax(100) == 13
     print("TEST TAX PASSED")
+
+
+def test_calculate_tax_integer_cents():
+    assert calculate_tax(10.5) == 1.36
+
+
+@pytest.mark.parametrize('income, expected', [
+                          (100, 13.0),
+                          (10.5, 1.36)
+])
+def test_calculate_tax_parametrized(
+    income, expected
+):
+    assert calculate_tax(income) == expected
 
 
 if __name__ == "__main__":
