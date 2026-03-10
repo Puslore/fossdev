8b0598480f3b27aea492e8d2882d7290abdc15ee
Puslore <mini.big10@mail.ru>
2026-03-10 14:38:40 +0800

    update setup file

--- testing/tdd/setup.py
+++ testing/tdd/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='income_tax',
+    name='ndfl',
     version='0.0.0',
     package_dir={'': 'src'},
     packages=find_packages(where='src')
