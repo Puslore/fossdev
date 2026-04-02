58374f758f9aacd0ae8a459f9d9ebac37d9eacfd
Puslore <mini.big10@mail.ru>
2026-04-02 12:42:11 +0800

    idk

--- testing/tdd/setup.py
+++ testing/tdd/setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='ndfl',
-    version='0.0.0',
+    name='ndfl-puslore',
+    version='0.0.2',
+    description='Simple NDFL calculator. The part of FOSS course.',
+    author='Puslore',
+    url='https://github.com/Puslore/fossdev',
     package_dir={'': 'src'},
     packages=find_packages(where='src')
 )
